df49051199b784dc73148d978dc309d92f10e45d
Tung Xuan <tungxuan101998@gmail.com>
2019-05-27 15:51:30 +0700

    13

--- recognition/views.py
+++ recognition/views.py
@@ -75,9 +75,9 @@ def index2(request, s=None):
     # l[2] la gia tri truy van
     l = s.split('=')
     file_path = os.path.join(settings.BASE_DIR, 'recognition', l[1])
-    if (l[3] != '' and len(os.listdir(train_folder)) == l[0] - 1):
+    if (l[2] == '-1' and l[3] == str(len(os.listdir(file_path)))):
+        print(': ' + l[3] + '---' + str(len(os.listdir(file_path))))
         image_path = convertfileimage(l[1])
-
         pathmodel = os.path.join(settings.BASE_DIR, 'recognition', 'knn.model')
         if (os.path.isfile(pathmodel)):
             model = joblib.load('knn.model')
